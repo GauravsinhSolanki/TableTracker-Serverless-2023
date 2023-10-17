c7354ef20ecb0cfaa1b61d275d8e10a71f72a27d
Arihant <arihant0996@gmail.com>
2023-10-17 20:54:56 -0300

    Update Ui for get menu items

--- frontend/src/Pages/Menu/ViewMenu.jsx
+++ frontend/src/Pages/Menu/ViewMenu.jsx
@@ -4,36 +4,56 @@ import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import { useEffect, useState } from "react";
+import config from "../../../../config.json";
+import axios from "axios";
+import "../../assets/scss/view-menu.css";
 
 function ViewMenu() {
   const { restaurantId } = useParams();
 
-  // Now, the 'id' variable contains the 'menu_id' from the URL
+  const [error, setError] = useState(false);
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      const url = config.Menu.getApiUrl;
+      try {
+        let result = await axios.get(url + restaurantId);
+        setItems(result.data.items);
+        console.log(result.data.items);
+      } catch (err) {
+        setError(err);
+      }
+    };
+    getData();
+  }, [restaurantId]);
 
   return (
-    <div className="container mt-5">
-        <h1>Hello {restaurantId}</h1>
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="https://lh3.googleusercontent.com/wAPeTvxh_EwOisF8kMR2L2eOrIOzjfA5AjE28W5asyfGeH85glwrO6zyqL71dCC26R63chADTO7DLOjnqRoXXOAB8t2f4C3QnU6o0BA" />
-            <Card.Body>
-                <Card.Title>Product Name</Card.Title>
-                <Card.Text>
-                    Product description goes here.
-                </Card.Text>
-                <Row>
-                    <Col sm="2">
-                        <Button variant="primary input-group-prepend">-</Button>
-                    </Col>
-                    <Col sm="4">
-                        <Form.Control type="qty" placeholder="Qty" value={0} />
-                    </Col>
-                    <Col sm="2">
-                        <Button variant="primary nput-group-append">+</Button>
-                    </Col>
-                </Row>
-            </Card.Body>
-        </Card>
-    </div>
+    <Row className="container mt-5">
+        {items && items.map((item, index) => (
+            <Card className="menu-item-cards" style={{ width: '18rem' }} key={index} id={"item-" + index}>
+                <Card.Img variant="top" src={item.img} />
+                <Card.Body>
+                    <Card.Title>{item.name}</Card.Title>
+                    <Card.Text>
+                        {item.description}
+                    </Card.Text>
+                    <Row className="quantity-row">
+                        <Col sm="2">
+                            <Button variant="primary input-group-prepend">-</Button>
+                        </Col>
+                        <Col sm="4">
+                            <Form.Control type="qty" placeholder="Qty" value={0} />
+                        </Col>
+                        <Col sm="2">
+                            <Button variant="primary input-group-append">+</Button>
+                        </Col>
+                    </Row>
+                </Card.Body>
+            </Card>
+        ))}
+    </Row>
   );
 }
 
