5e72ce9380733c4e13140d77d02f716b4bd5351c
Riya Patel <ry818465@dal.ca>
2023-10-19 15:28:55 -0300

    Subscribe Button

--- frontend/src/Pages/LandingPage/LandingPage.jsx
+++ frontend/src/Pages/LandingPage/LandingPage.jsx
@@ -1,18 +1,32 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, Text, Button, VStack } from '@chakra-ui/react';
 import React from 'react';
 import { useMediaQuery } from 'react-responsive';
 
 function LandingPage() {
     const isMobile = useMediaQuery({ query: '(max-width: 1080px)' });
+
     return (
-        isMobile ?
-            <Flex w="100%" minHeight="90vh" backgroundColor="#000C66" flexDir="column" alignItems="center" justifyContent="start">
-                <Text color="white">This is the Landing Page</Text>
+        <>
+            {isMobile ? (
+                <Flex w="100%" minHeight="90vh" backgroundColor="#000C89" flexDir="column" alignItems="center" justifyContent="start">
+                    <Text color="white">This is the Landing Page</Text>
+                </Flex>
+            ) : (
+                <Flex w="100%" minHeight="90vh" backgroundColor="#000C66" alignItems="center" justifyContent="space-evenly">
+                    <Text color="white">This is the Landing Page</Text>
+                </Flex>
+            )}
+
+            {/* Footer */}
+            <Flex w="100%" backgroundColor="#000C44" alignItems="center" justifyContent="center" py={4}>
+                <VStack spacing={4}>
+                    <Button colorScheme="teal" variant="solid">
+                        Subscribe
+                    </Button>
+                    <Text color="white" fontSize="sm">© 2023 Table Reservation App. All Rights Reserved.</Text>
+                </VStack>
             </Flex>
-            :
-            <Flex w="100%" minHeight="90vh" backgroundColor="#000C66" alignItems="center" justifyContent="space-evenly">
-                <Text color="white">This is the Landing Page</Text>
-            </Flex>
+        </>
     );
 }
 
