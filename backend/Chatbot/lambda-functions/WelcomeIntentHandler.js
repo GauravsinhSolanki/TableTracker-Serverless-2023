a9c65c1f6fb65ddf6be75d08876fce0ca3918fef
GauravsinhSolanki <solankigbs11@gmail.com>
2023-10-24 14:54:36 -0300

    Intial process setup for the ChataBot Configuration.

---
+++ backend/Chatbot/lambda-functions/WelcomeIntentHandler.js
@@ -0,0 +1,47 @@
+import {
+  LexRuntimeV2Client,
+  RecognizeTextCommand,
+} from "@aws-sdk/client-lex-runtime-v2";
+
+export async function handler(event) {
+  console.log(event);
+
+  const response = {
+    sessionState: {
+      sessionAttributes: {
+        key: "value", 
+      },
+    },
+    messages: [
+      {
+        contentType: "PlainText",
+        content:
+          "Welcome to our Table Reservation Service! How can I assist you today?",
+      },
+    ],
+  };
+
+  const lexClient = new LexRuntimeV2Client({ region: "us-east-1" });
+  const params = {
+    botAliasId: "TSTALIASID",
+    botId: "GALF77JWIJ",
+    localeId: "en_US",
+    sessionId: event.sessionId,
+    messages: response.messages,
+  };
+
+  try {
+    const command = new RecognizeTextCommand(params);
+    await lexClient.send(command);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Lex response sent successfully." }),
+    };
+  } catch (error) {
+    console.error("Error sending Lex response:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal Server Error" }),
+    };
+  }
+}
