2395228aec27bdee357d0bf10bd99af345b17d85
Riya Patel <ry818465@dal.ca>
2023-10-26 17:53:09 -0300

    Merge issues solved

--- frontend/src/Pages/LandingPage/LandingPage.jsx
+++ frontend/src/Pages/LandingPage/LandingPage.jsx
@@ -1,47 +1,19 @@
-import { Flex, Text, Button, VStack, Input, HStack } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
+import React from 'react';
 import { useMediaQuery } from 'react-responsive';
+import { theme } from '../../../theme';
 
 function LandingPage() {
     const isMobile = useMediaQuery({ query: '(max-width: 1080px)' });
-    const [email, setEmail] = useState('');
-
-    async function handleSubscribe() {
-        try {
-            const response = await fetch('arn:aws:execute-api:us-east-1:247203851890:m7hepnrask/*/OPTIONS/offers', {
-                method: 'POST',
-                body: JSON.stringify({ email: email }),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });            
-            const data = await response.json();
-            console.log(data);
-            if (response.ok) {
-                // Handle success, for example, show a success message
-                alert('Subscription successful! Thank you.');
-                setEmail(''); // Clear the email input field on success
-            } else {
-                // Handle error, for example, show an error message
-                alert('Subscription failed. Please try again later.');
-            }
-        } catch (error) {
-            console.error("There was an error subscribing:", error);
-            // Handle error 
-            alert('An error occurred. Please try again later.');
-        }
-    }
     return (
-        <>
-            {isMobile ? (
-                <Flex w="100%" minHeight="90vh" backgroundColor="#000C89" flexDir="column" alignItems="center" justifyContent="start">
-                    <Text color="white">This is the Landing Page</Text>
-                </Flex>
-            ) : (
-                <Flex w="100%" minHeight="90vh" backgroundColor="#000C66" alignItems="center" justifyContent="space-evenly">
-                    <Text color="white">This is the Landing Page</Text>
-                </Flex>
-            )}
-        </>
+        isMobile ?
+            <Flex w="100%" minHeight="90vh" backgroundColor={theme.primaryBackground} flexDir="column" alignItems="center" justifyContent="start">
+                <Text color="white">This is the Landing Page</Text>
+            </Flex>
+            :
+            <Flex w="100%" minHeight="90vh" backgroundColor={theme.primaryBackground} alignItems="center" justifyContent="space-evenly">
+                <Text color="white">This is the Landing Page</Text>
+            </Flex>
     );
 }
 
