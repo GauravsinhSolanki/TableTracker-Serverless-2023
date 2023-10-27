4e9e574bfd005e09a98af3b67dc7892f11a2c2ac
Rashmi <rs216961@dal.ca>
2023-10-27 03:48:12 -0300

    navbar changes

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,16 @@
-import { Box, Flex, Text, useDisclosure } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 import React from 'react';
 import { useMediaQuery } from 'react-responsive';
 import { NavLink } from 'react-router-dom';
 import { theme } from '../../theme';
 
+function logoutUser() {
+    sessionStorage.clear();
+}
+
 function Navbar() {
     const isMobile = useMediaQuery({ query: '(max-width: 1080px)' });
-    const { isOpen, onOpen, onClose } = useDisclosure();
+    const user = sessionStorage.getItem("userDetails");
 
     return (
         isMobile ?
@@ -16,11 +20,34 @@ function Navbar() {
             :
             <Flex as="nav" alignItems="center" justify="space-between" h="10vh" w="100%" backgroundColor={theme.secondaryBackground}>
                 <Text color="white">This is Navbar</Text>
+                {!user ? 
+                    
+                (<>
+                    <Box>
+                    <NavLink to='/user/login'>
+                        <Text fontWeight="medium" color="white" fontSize="lg">Login</Text>
+                    </NavLink>
+                </Box>
+                <Box>
+                <NavLink to='/user/signup'>
+                    <Text fontWeight="medium" color="white" fontSize="lg">Signup</Text>
+                </NavLink>
+                </Box>
+                </>)
+                :
+               ( 
+                <>
                 <Box>
                     <NavLink to='/restaurantList'>
                         <Text fontWeight="medium" color="white" fontSize="lg">Restaurants</Text>
                     </NavLink>
                 </Box>
+                <Box>
+                <NavLink to='/user/login' onClick={logoutUser}>
+                    <Text fontWeight="medium" color="white" fontSize="lg">Logout</Text>
+                </NavLink>
+                </Box>
+                </>)}
             </Flex>
     );
 }
