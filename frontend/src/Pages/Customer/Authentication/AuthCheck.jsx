e7489836dc219f062e24b59af309f7a9b49969e8
Rashmi <rs216961@dal.ca>
2023-10-27 04:02:41 -0300

    formatting

--- frontend/src/Pages/Customer/Authentication/AuthCheck.jsx
+++ frontend/src/Pages/Customer/Authentication/AuthCheck.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AuthCheck = (WrappedComponent) => {
-    const AuthenticatedComponent = (props) => {
-        const navigate = useNavigate();
+  const AuthenticatedComponent = (props) => {
+    const navigate = useNavigate();
 
-        useEffect(() => {
-            const user = sessionStorage.getItem("userDetails");
-            if (!user) {
-                navigate('/user/login');
-            }
-        }, [navigate, props.path]);
+    useEffect(() => {
+      const user = sessionStorage.getItem("userDetails");
+      if (!user) {
+        navigate("/user/login");
+      }
+    }, [navigate, props.path]);
 
-        return <WrappedComponent {...props} />;
-    };
+    return <WrappedComponent {...props} />;
+  };
 
-    return AuthenticatedComponent;
+  return AuthenticatedComponent;
 };
--- frontend/src/Pages/Customer/Authentication/firebase.js
+++ frontend/src/Pages/Customer/Authentication/firebase.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDglaFe88jPa7V7aTzl81aHwY_BgqgRehE",
   authDomain: "csci5410-f23-sdp3.firebaseapp.com",
@@ -16,7 +11,6 @@ const firebaseConfig = {
   measurementId: "G-NPHKJK1T4V"
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider()
