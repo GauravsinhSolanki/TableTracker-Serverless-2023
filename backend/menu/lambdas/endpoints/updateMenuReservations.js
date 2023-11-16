b4e7b4cd2d20c378cd73f9dd09fc6732f2eb957d
Arihant <arihant0996@gmail.com>
2023-11-15 22:32:08 -0400

    Changes for firestore and enhancements

--- backend/menu/lambdas/endpoints/updateMenuReservations.js
+++ backend/menu/lambdas/endpoints/updateMenuReservations.js
@@ -1,38 +1,48 @@
 'use strict';
 const Responses = require('../common/API_Responses');
-const Dynamo = require('../common/Dynamo');
+const admin = require("firebase-admin");
+const serviceAccount = require("./sdp3-firestore.json"); // file path for service account credentials
 
-const tableName = process.env.menuReservationsTableName;
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://csci5410-f23-sdp3.firebaseio.com", // Firebase project URL
+});
 
 module.exports.handler = async (event) => {
 
-    if(!event.pathParameters || !event.pathParameters.Id) {
-        // failed to get as no id provided
+    try {
+        const db = admin.firestore();
+
+        if(!event.pathParameters || !event.pathParameters.Id) {
+            // failed to get as no id provided
+            return Responses._400({
+                message: 'No id specified'
+            });
+        }
+    
+        const collectionRef = db.collection('MenuReservations');
+
+        // Get the document to update
+        const querySnapshot = await collectionRef.where('id', '==', event.pathParameters.Id).get();
+
+        querySnapshot.forEach(async (doc) => {
+            const docId = doc.id;
+            const data = doc.data();
+            data.items = JSON.parse(event.body);
+
+            // Update the document with the modified data
+            await collectionRef.doc(docId).set(data);
+        });
+
+        // Return a success response
+        return { 
+            statusCode: 200, body: 'Menu reservation updated successfully' 
+        };
+    } catch (error) {
+        console.log(error);
         return Responses._400({
-            message: 'No id specified'
+            message: "Error updating menu reservation",
+            error: error.message,
         });
     }
-
-    const updatedVal = JSON.parse(event.body);
-    
-    const params = {
-        TableName: tableName,
-        Key: {
-            // Menu reservation Id
-            id : event.pathParameters.Id
-        },
-        UpdateExpression: 'SET #key = :updateKeyValue',
-        ExpressionAttributeValues: {
-            ':updateKeyValue' : updatedVal
-        }, 
-        ExpressionAttributeNames: {
-          "#key": "items"
-        }
-    };
-
-    const reservation = await Dynamo.update(params);
-
-    return Responses._200({
-        message : 'Menu reservation updated successfully'
-    });
 };
